b0e4a05b4623ec7b5e6ac2221e1e771e58f6c867
jaspermarsman <jaspermarsman@gmail.com>
2021-04-29 19:43:55 +0200

    Finished function for tvs to sell

--- main.js
+++ main.js
@@ -161,3 +161,67 @@ const inventory = [
     sold: 8,
   },
 ];
+
+// Opdracht 1a: Hoeveel exemplaren moeten we nog verkopen? Schrijf een functie die dit berekent.
+// - [ ] make a variable to store the total
+// - [ ] inventory.map(~ callback here ~)
+// - [ ] callback schrijven voor map
+// - [ ] parameter: television
+// - [ ] log the data we need: television.sold, television.originalStock
+// - [ ] do math: const tvsToSell = originalStock - sold
+//     - [ ] console.log(tvsToSell)
+// - [ ] return the value from the callback: tvsToSell
+// - [ ] We should have something like: `[3, 4, 9, 11]`
+// - [ ] Loop over the array of numbers and add them together (possibly google it)
+
+function calculateTvToSellCount() {
+  const tvSoldArray = inventory.map((television) => {
+    return television.originalStock - television.sold;
+  });
+
+  let total = 0;
+
+  for (let i = 0; i < tvSoldArray.length; i++) {
+    const tvSoldNumber = tvSoldArray[i];
+    total = total + tvSoldNumber;
+    // of total += tvSoldArray[i];
+    console.log("in for loop ", total);
+  }
+  return total;
+}
+console.log("total", calculateTvToSellCount());
+
+// Opdracht 1b: Zorg ervoor dat dit aantal in het rood wordt weergegeven op de pagina
+// make variable for the new element
+// create an element (document.createElement)
+//set content by changing elements: element.textContent = total
+// appendChild (stick it on the page)
+
+const tvToSellElement = document.createElement("h1");
+
+// const tvType1 = document.createElement("p");
+// tvType1.textContent = tvType[0];
+// document.body.appendChild(tvType1);
+//
+// const inventory2 = document.createElement("p");
+// inventory2.textContent = currentStock[0];
+// document.body.appendChild(inventory2);
+
+// function newTable() {
+//   const table = document.getElementById("tvTable");
+//   const newRow = table.insertRow(-1)
+//   const newCell = newRow.insertCell(-1);
+//   const tvType1 = document.createTextNode(tvType[0]);
+//   newCell.appendChild(tvType1);
+// }
+//newTable("tvTable");
+
+// element.innerText --> tekst aanpassen
+// element.remove() --> verwijderen
+// document.createElement("p); nieuw element aanmaken
+// document.body.apendChild(nieuwe-variabele); (op de pagina plakken)
+
+// element selecteren: const button = document.getElementById("best-button");
+// listen to click events and handle to it:
+// button.addEventListener("click", () => { console.log("handle it"); } );
+
